b37bf81af5bcb10341c50786b4b4128a9eaccbfc
SASHAMATU <aleksandrmatockin3@gmail.com>
2024-04-26 19:33:21 +0200

    п

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,4 @@
+'use strict';
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
     const makeTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
